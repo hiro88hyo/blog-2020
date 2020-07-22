33a2a71202f5617e92b6761889d2dfc7355218fa
hiroyuki <hiro88hyo@gmail.com>
2020-07-22 20:56:39 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  url: 'https://blog.imagesensor.club',
+  pathPrefix: '/',
+  title: 'Admire The Imagesensor',
+  subtitle: 'テック',
+  copyright: '© All rights reserved.',
+  disqusShortname: 'imagesensor',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-119667850-1',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+/*    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    } */
+  ],
+  author: {
+    name: 'John Doe',
+    photo: '/photo.jpg',
+    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
